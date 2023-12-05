e6d52393366b4f632bf545a7175d9b66673c8c37
xavierabreu <xavierabreu00@gmail.com>
2023-12-05 17:17:45 -0500

    Almost Finished... Forgot to commit Earlier

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const todoList = document.getElementById('todo-list');
+    const newTodoInput = document.getElementById('new-todo');
+    const addTodoButton = document.getElementById('add-todo');
+
+    addTodoButton.addEventListener('click', function () {
+        const todoText = newTodoInput.value.trim();
+
+        if (todoText !== '') {
+            // Create a new list item
+            const listItem = document.createElement('li');
+            listItem.textContent = todoText;
+
+            // Add click event to remove the item
+            listItem.addEventListener('click', function () {
+                todoList.removeChild(listItem);
+            });
+
+            // Add the new list item to the bottom of the list
+            todoList.appendChild(listItem);
+
+            // Clear the input box
+            newTodoInput.value = '';
+        }
+    });
+});
